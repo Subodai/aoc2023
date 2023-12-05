2012676a4c10aa1dc5df4043641fe1308c89e94e
Mark Thompson <subodai85@gmail.com>
2023-12-05 11:34:38 +0000

    Tidy up Day 2 a bit

--- 2/day2.go
+++ 2/day2.go
@@ -13,16 +13,12 @@ import (
 func main() {
 	data := new(Data)
 	processInput("input.txt", data)
-	//fmt.Printf("\n\nRAW DATA: %+v\n", data)
 	processData(data)
-	//fmt.Printf("\n\nComputed DATA: %+v\n", data)
-
 	var (
 		red, _   = strconv.Atoi(os.Args[1])
 		green, _ = strconv.Atoi(os.Args[2])
 		blue, _  = strconv.Atoi(os.Args[3])
 	)
-	//fmt.Printf("Red: %d, Green: %d, Blue: %d\n", red, green, blue)
 	validateData(data, red, green, blue)
 	fmt.Printf("\n\nValidated DATA: %+v\n", data.Stats)
 	total, power := getTotal(data)
@@ -80,13 +76,12 @@ func processInput(filename string, data *Data) {
 		// get position of semicolon
 		start := strings.Index(line, ":")
 		id, _ := strconv.Atoi(line[5:start])
-		//fmt.Printf("Game ID: %d \n", id)
+
 		game := new(Game)
 		game.ID = id
 
 		// Need to get the content
 		content := line[start+2:]
-		//fmt.Print(content, "\n")
 
 		// Need to split the content into sets
 		sets := strings.SplitN(content, "; ", -1)
@@ -94,18 +89,14 @@ func processInput(filename string, data *Data) {
 		// Need to split the sets into cubes
 		for _, set := range sets {
 			s := new(Set)
-			//fmt.Print(set, "\n")
 			cubes := strings.SplitN(set, ", ", -1)
 
 			for _, cube := range cubes {
 				c := new(Cube)
-				//fmt.Print(cube, "\n")
 				c.Colour = strings.Join(lettersOnly.FindAllString(cube, -1), "")
 				c.Count, _ = strconv.Atoi(strings.Join(digitsOnly.FindAllString(cube, -1), ""))
-				//fmt.Printf("%+v\n", c)
 				s.Cubes = append(s.Cubes, *c)
 			}
-			//fmt.Printf("%+v\n", s)
 			game.Sets = append(game.Sets, *s)
 		}
 		data.Games = append(data.Games, *game)
@@ -147,6 +138,7 @@ func processData(data *Data) {
 	}
 }
 
+// validateData checks our processed data against the maximum number of each coloured cube
 func validateData(data *Data, red int, green int, blue int) {
 	for i, stat := range data.Stats {
 		valid := true
@@ -157,6 +149,7 @@ func validateData(data *Data, red int, green int, blue int) {
 	}
 }
 
+// getTotal processes the total of valid games, and also totals up the powers of each game
 func getTotal(data *Data) (total int, totalPower int) {
 	total = 0
 	totalPower = 0
